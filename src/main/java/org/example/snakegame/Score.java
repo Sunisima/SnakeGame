94d95d4b9ac63e3af3df64d74dd7c615baf96d43
TobiasFalck <TobiasFalck13@gmail.com>
2025-05-06 12:46:51 +0200

    Score class

--- src/main/java/org/example/snakegame/Score.java
+++ src/main/java/org/example/snakegame/Score.java
@@ -1,4 +1,37 @@
 package org.example.snakegame;
 
-public class Score {
+public class Score
+{
+    // Variable to store the score
+    private int value;
+
+    // Constructor
+    public Score()
+    {
+        this.value = 0;
+    }
+
+    // Update the score when the snake eats a food
+    public void updateScore(int points)
+    {
+        this.value += points;
+    }
+
+    // Get current score value
+    public int getScore()
+    {
+        return this.value;
+    }
+
+    // Resets the score to 0
+    public void resetScore()
+    {
+        this.value = 0;
+    }
+
+    // Displays score
+    public String displayScore()
+    {
+        return "Score: " + this.value;
+    }
 }
